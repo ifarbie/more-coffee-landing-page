f14c65b9eb1e6b7bbc20c0a1097e14b1d5bbe67c
ifarbie <farizrifkyberliano@gmail.com>
2024-09-28 09:15:45 +0700

    feat: clean the js code and add max width

--- js/script.js
+++ js/script.js
@@ -1,66 +1,68 @@
-const navigationList = ["Home", "About", "Contact"];
-const socialMedias = ["Instagram", "TikTok", "Youtube", "X-Twitter"]
-addNavigation(navigationList);
-addHamburgerMenuButton();
-addSocialMedia(socialMedias);
+const navigationList = ['Home', 'About', 'Contact'];
+const socialMedias = ['Instagram', 'TikTok', 'Youtube', 'X-Twitter'];
 
-const hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener('click', () => {
-    const headerNav = document.querySelector("header nav");
+main();
+function main() {
+  addNavigation(navigationList);
+  addHamburgerMenuButton();
+  addSocialMedia(socialMedias);
+}
 
-    hamburger.classList.toggle("hamburger-active");
-    headerNav.classList.toggle("nav-active");
-})
-
-// Fungsi untuk menambahkan Navigasi ke halaman web
 function addNavigation(navigation) {
-    const headerNav = document.querySelector("header nav");
-    const ul = document.createElement("ul");
+  const headerNav = document.querySelector('header nav');
+  const ul = document.createElement('ul');
 
-    for (const nav of navigation) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.textContent = nav.toUpperCase();
+  for (const nav of navigation) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = nav.toUpperCase();
 
-        if (nav === "Home") a.setAttribute("href", "#");
-        else a.setAttribute("href", `#${nav.toLowerCase()}`);
+    if (nav === 'Home') a.setAttribute('href', '#');
+    else a.setAttribute('href', `#${nav.toLowerCase()}`);
 
-        li.appendChild(a);
-        ul.appendChild(li);
-    }
-    headerNav.appendChild(ul);
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  headerNav.appendChild(ul);
 }
 
 function addHamburgerMenuButton() {
-    const header = document.querySelector("header");
-    const button = document.createElement("button");
-    button.classList.add("hamburger");
-    button.setAttribute("type", "button");
+  const header = document.querySelector('header');
+  const button = document.createElement('button');
+  button.classList.add('hamburger');
+  button.setAttribute('type', 'button');
 
-    for (let i = 1; i <= 3; i++) {
-        const span = document.createElement("span");
-        span.classList.add('hamburger-line');
-        button.appendChild(span)
-    }
+  for (let i = 1; i <= 3; i++) {
+    const span = document.createElement('span');
+    span.classList.add('hamburger-line');
+    button.appendChild(span);
+  }
 
-    header.appendChild(button);
+  button.addEventListener('click', () => {
+    const headerNav = document.querySelector('header nav');
+
+    button.classList.toggle('hamburger-active');
+    headerNav.classList.toggle('nav-active');
+  });
+
+  header.appendChild(button);
 }
 
 function addSocialMedia(socialMedias) {
-    const elSocialMedia = document.querySelector(".social-media");
-    const ul = document.createElement("ul");
+  const elSocialMedia = document.querySelector('.social-media');
+  const ul = document.createElement('ul');
 
-    for (socialMedia of socialMedias) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.setAttribute("href", "#");
-        const i = document.createElement("i");
-        i.setAttribute("class", `fa-brands fa-${socialMedia.toLowerCase()}`);
+  for (socialMedia of socialMedias) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.setAttribute('href', '#');
+    const i = document.createElement('i');
+    i.setAttribute('class', `fa-brands fa-${socialMedia.toLowerCase()}`);
 
-        a.appendChild(i)
-        li.appendChild(a);
-        ul.appendChild(li)
-    }
+    a.appendChild(i);
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
 
-    elSocialMedia.appendChild(ul);
+  elSocialMedia.appendChild(ul);
 }
